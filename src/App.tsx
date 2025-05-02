7658dfae09f02415a229e760a31ee2f92c0b134e
Neil Davies <neil.davies@thrivelearning.com>
2025-05-02 13:57:14 +0100

    create a basic form to log in

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import bryxLogo from '/bryx-ltd.png'
 import './App.css'
 
 function App() {
+
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={bryxLogo} className="logo" alt="Bryx logo" />
+          <img src={bryxLogo} className="logo" alt="Bryx logo"/>
         </a>
       </div>
-      <h1>Bryx Ltd.
-      </h1>
+      <h1>Log in to Bryx</h1>
+      <form>
+        <input type="email" placeholder="email"/>
+        <input type="password" placeholder="password"/>
+        <button type="submit">Submit</button>
+        <p>
+          Don't have an account? <a href="/register">Register here</a>
+        </p>
+      </form>
     </>
   )
 }
